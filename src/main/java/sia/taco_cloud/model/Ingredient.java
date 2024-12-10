f9e5a56159c5543097956fcb236710ad052fabfb
kornevVlad <vlad1990@yandex.ru>
2024-12-10 15:29:44 +0300

    Реализация data jpa

--- src/main/java/sia/taco_cloud/model/Ingredient.java
+++ src/main/java/sia/taco_cloud/model/Ingredient.java
@@ -1,29 +1,17 @@
 package sia.taco_cloud.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
-@Table
-@AllArgsConstructor
-@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
-public class Ingredient implements Persistable<String> {
+@Entity
+public class Ingredient {
 
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
-
-    @Override
-    public boolean isNew() {
-        return false;
-    }
-
+    private String id;
+    private String name;
+    private Type type;
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
--- src/main/java/sia/taco_cloud/model/Taco.java
+++ src/main/java/sia/taco_cloud/model/Taco.java
@@ -1,28 +1,32 @@
 package sia.taco_cloud.model;
 
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+        @NotNull
+        @Size(min = 3, message = "Поле не должно быть пустым, введите свое имя")
+        private String name;
+        private Date createdAt = new Date();
+        @Size(min = 1, message = "Выберите хотя бы один ингредиент")
+        @ManyToMany()
+        private List<Ingredient> ingredients = new ArrayList<>();
 
-    @Id
-    private Long id;
-    private Date createdAt = new Date();
-
-    @NotNull
-    @Size(min = 3, message = "Поле не должно быть пустым, введите свое имя")
-    private String name;
-
-    @NotNull
-    @Size(min = 1, message = "Выберите хотя бы один ингредиент")
-    private List<IngredientRef> ingredients = new ArrayList<>();
+        public void addIngredient(Ingredient ingredient) {
+            this.ingredients.add(ingredient);
+        }
 }
+
+
